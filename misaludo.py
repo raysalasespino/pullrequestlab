02cb92a362a05b5eb31ae00edfdffdd2f1b7ed6e
Ray Salas Espino <csalas27932@hotmail.com>
2021-10-14 15:04:36 -0500

    se actualizaron comillas faltantes

--- misaludo.py
+++ misaludo.py
@@ -2,4 +2,4 @@ nombre = "Ray Salas"
 edad = 28
 
 print ("Mi nombre es {} y tengo una de {} ".format(nombre,edad))
-print (f"Mi nombre es {nombre} y tengo una edad de {edad})
+print (f"Mi nombre es {nombre} y tengo una edad de {edad}")
